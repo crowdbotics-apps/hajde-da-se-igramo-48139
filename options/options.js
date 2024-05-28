54a18f731e21e49af2f8982630010909f23215bd
Crowdbotics <team@crowdbotics.com>
2024-05-28 06:56:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hajde-da-se-igramo-48139",
+  url: "https://hajde-da-se-igramo-48139.botics.co",
+  api: "https://hajde-da-se-igramo-48139.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
